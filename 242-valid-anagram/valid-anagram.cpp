cbf0e6cefc01d51a3ac43d5b4f7d39d979a989da
Om Shukla <oms226946@gmail.com>
2026-07-03 15:16:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (49.56%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        int n = s.length();
+        int counts[26] = {0};
+        for (int i = 0; i < n; i++) { 
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++)
+            if (counts[i]) return false;   //c++ me agar ciunts 0 ho iska matlab false hota hai 
+        return true;
+    }
+};
