41ef36957b6573f34024c1afabcd5f14d66a0c7c
Om Shukla <oms226946@gmail.com>
2026-07-18 10:18:19 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (31.74%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
@@ -1,28 +1,28 @@
-class Solution {
-public:
-    int findGCD(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return gcd(nums[0], nums.back());
-    }
-};
+// class Solution {
+// public:
+//     int findGCD(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         return gcd(nums[0], nums.back());
+//     }
+// };
 
 
 
 //anathor solution 
 
-// class Solution {
-// public:
-//     int findGCD(vector<int>& nums) {
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
 
-//         int mn = INT_MAX;
-//         int mx = INT_MIN;
+        int mn = INT_MAX;
+        int mx = INT_MIN;
 
-//         for (int x : nums) {
-//             mn = min(mn, x);
-//             mx = max(mx, x);
-//         }
+        for (int x : nums) {
+            mn = min(mn, x);
+            mx = max(mx, x);
+        }
 
-//         // GCD of the smallest and largest element.
-//         return gcd(mn, mx);
-//     }
-// };
+        // GCD of the smallest and largest element.
+        return gcd(mn, mx);
+    }
+};
