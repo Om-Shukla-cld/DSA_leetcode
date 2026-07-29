39c4b5e40eae785f147eb7b53d7f76edf0d3dd67
Om Shukla <oms226946@gmail.com>
2026-07-29 16:52:55 +0530

    Time: 333 ms (5.05%) | Memory: 21.8 MB (71.21%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -24,3 +24,21 @@ public:
         return -1;
     }
 };
+
+
+//optimal
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         int expectedSum = (n * (n + 1)) / 2;  // Sum of first n natural numbers
+//         int actualSum = 0;
+
+//         for(int i = 0; i < n; i++) {
+//             actualSum += nums[i];  // Adding all elements of the array
+//         }
+
+//         return expectedSum - actualSum;  // Missing number is the difference
+//     }
+// };
