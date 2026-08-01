10e080fd2ca8cf059fcf1dabf2dcc36aa544dd73
Om Shukla <oms226946@gmail.com>
2026-08-01 22:24:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (81.88%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -7,9 +7,9 @@ public:
         
         while (j >= 0) {
             if (i >= 0 && nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
+         nums1[k--] = nums1[i--];    
             } else {
-                nums1[k--] = nums2[j--];
+                nums1[k--] = nums2[j--];//nums[5]==6   isko last me rakhunga
             }
         }
     }
