3c7d24f96473af9480f853a88580f1d2b648a5be
Om Shukla <oms226946@gmail.com>
2026-08-15 15:16:25 +0530

    Time: 14 ms (22.68%) | Memory: 7.7 MB (65.83%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n <= 1)
-            return n;
 
+        // Base case
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        // Recursive case
         return fib(n - 1) + fib(n - 2);
     }
 };
