120e87dc814ad4d9fa9c81edbb4c7ca8ee2de14e
Om Shukla <oms226946@gmail.com>
2026-09-15 00:31:56 +0530

    Time: 0 ms (100.00%) | Memory: 99.1 MB (70.93%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+         int temp1 = edges[0][0];
+        int temp2 = edges[0][1];
+        if(edges[1][0]==temp1 || edges[1][1]==temp1) return temp1;
+        return temp2;
+        
+    }
+};
